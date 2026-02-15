3451a9c0590a92f606eaf835a67bd37c43cf54c3
Do1415162 <buidinhdo152@gmail.com>
2026-02-15 16:33:51 +0700

    Cap nhat danh muc: Chi con 4 danh muc chinh (PS4, PS5, Nintendo Switch, Xbox)

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -16,98 +16,31 @@ class CategorySeeder extends Seeder
     {
         $categories = [
             [
-                'name' => 'PlayStation',
-                'slug' => 'playstation',
-                'description' => 'Máy PlayStation từ PS2 đến PS5',
+                'name' => 'PlayStation 4',
+                'slug' => 'ps4',
+                'description' => 'Máy PlayStation 4, PS4 Slim, PS4 Pro',
                 'order' => 1,
                 'is_active' => true
             ],
             [
-                'name' => 'PlayStation 2 (PS2)',
-                'slug' => 'playstation-2',
-                'description' => 'Máy PlayStation 2 chính hãng',
-                'parent_id' => 1,
-                'order' => 1,
-                'is_active' => true
-            ],
-            [
-                'name' => 'PlayStation 3 (PS3)',
-                'slug' => 'playstation-3',
-                'description' => 'Máy PlayStation 3 chính hãng',
-                'parent_id' => 1,
+                'name' => 'PlayStation 5',
+                'slug' => 'ps5',
+                'description' => 'Máy PlayStation 5, PS5 Slim, PS5 Pro và Game PS5',
                 'order' => 2,
                 'is_active' => true
             ],
             [
-                'name' => 'PlayStation 4 (PS4)',
-                'slug' => 'playstation-4',
-                'description' => 'Máy PlayStation 4 và PS4 Pro',
-                'parent_id' => 1,
+                'name' => 'Nintendo Switch',
+                'slug' => 'nintendo-switch',
+                'description' => 'Máy Nintendo Switch và Game Nintendo Switch',
                 'order' => 3,
                 'is_active' => true
             ],
             [
-                'name' => 'PlayStation 5 (PS5)',
-                'slug' => 'playstation-5',
-                'description' => 'Máy PlayStation 5 và PS5 Pro',
-                'parent_id' => 1,
+                'name' => 'Xbox',
+                'slug' => 'xbox',
+                'description' => 'Máy Xbox Series X/S và phụ kiện Xbox',
                 'order' => 4,
-                'is_active' => true
-            ],
-            [
-                'name' => 'Game PlayStation',
-                'slug' => 'game-playstation',
-                'description' => 'Game cho các dòng PlayStation',
-                'order' => 2,
-                'is_active' => true
-            ],
-            [
-                'name' => 'Máy Nintendo Switch',
-                'slug' => 'may-nintendo-switch',
-                'description' => 'Máy Nintendo Switch chính hãng',
-                'order' => 3,
-                'is_active' => true
-            ],
-            [
-                'name' => 'Game Nintendo Switch',
-                'slug' => 'game-nintendo-switch',
-                'description' => 'Game Nintendo Switch chính hãng',
-                'order' => 4,
-                'is_active' => true
-            ],
-            [
-                'name' => 'Máy Xbox',
-                'slug' => 'may-xbox',
-                'description' => 'Máy Xbox Series X/S chính hãng',
-                'order' => 5,
-                'is_active' => true
-            ],
-            [
-                'name' => 'iPhone',
-                'slug' => 'iphone',
-                'description' => 'iPhone chính hãng VN/A',
-                'order' => 6,
-                'is_active' => true
-            ],
-            [
-                'name' => 'iPad',
-                'slug' => 'ipad',
-                'description' => 'iPad chính hãng Apple',
-                'order' => 7,
-                'is_active' => true
-            ],
-            [
-                'name' => 'Tay cầm',
-                'slug' => 'tay-cam',
-                'description' => 'Tay cầm chơi game',
-                'order' => 8,
-                'is_active' => true
-            ],
-            [
-                'name' => 'Phụ kiện',
-                'slug' => 'phu-kien',
-                'description' => 'Phụ kiện gaming',
-                'order' => 9,
                 'is_active' => true
             ],
         ];
